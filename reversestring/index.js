3ee71183bfc951f12dd23c01e2d5a91ee2b91856
MoMoZin <khinmomozin@gmail.com>
2023-05-24 09:57:17 -0600

    reverse string using for of  method

--- reversestring/index.js
+++ reversestring/index.js
@@ -18,17 +18,33 @@
 //   return resArray.join("");
 // }
 
-// method 2
+// // method 2
+// function reverse(str) {
+
+//   // for each character in the string, add to the new string, starting from last char index 
+//   let result = "";
+//   for (let i = str.length - 1; i >= 0; i--) {
+//     result += str[i];
+//   }
+
+//   return result;
+// }
+
+// method 3
 function reverse(str) {
 
-  // for each character in the string, add to the new string, starting from last char index 
+  // for each character in the string, take the char and add it to the start of the new string
   let result = "";
-  for (let i = str.length - 1; i >= 0; i--) {
-    result += str[i];
+  for (let char of str) {
+    result = char + result;
   }
 
   return result;
 }
+
+
+
+
 
 
 module.exports = reverse;
