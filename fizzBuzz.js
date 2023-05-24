120e457d6a11907dd202204bdf3f80c74194af33
MoMoZin <khinmomozin@gmail.com>
2023-05-24 08:58:29 -0600

    add question description and fix print statement

--- fizzBuzz.js
+++ fizzBuzz.js
@@ -1,3 +1,9 @@
+// Write a program that prints the numbers from 1 to 100.
+// But for multiples of three print "Fizz" instead of the number and
+// for tthe multiples of five print "Buzz".
+// for numbers which are multiples of both three and five, print "FizzBuzz"
+
+
 const printFizBuzz = () => {
   for (i = 1; i <= 100; i++) {
     let result = "";
@@ -7,9 +13,7 @@ const printFizBuzz = () => {
     if (i % 5 === 0) {
       result += "Buzz";
     }
-    if (result.length > 0) {
-      console.log("result: ", result);
-    }
+    result.length > 0 ? console.log("result: ", result) : console.log("number: ", i);
   }
 };
 
