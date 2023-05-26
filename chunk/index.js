7db45852e1a7437fa69675dc43c70dc9cca5b42d
MoMoZin <khinmomozin@gmail.com>
2023-05-26 16:06:09 -0600

    split array into 2d array chunk by provided array size

--- chunk/index.js
+++ chunk/index.js
@@ -8,6 +8,32 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+function chunk(array, size) {
+  //create outer array container
+  let outerArr = [];
+
+  //create inner array container for split element by size provided
+  let innerArr = [];
+
+  array.forEach(val => {
+    //add item to inner container
+    innerArr.push(val);
+
+    // check if cotainer size is eqal to than provided size
+    if (innerArr.length === size) {
+      //add inner container to outer container
+      outerArr.push(innerArr);
+      //reset the inner container
+      innerArr = [];
+    }
+  });
+
+  //if last inner container was less than the size limit, add it directly to outer container here
+  if (innerArr.length > 0) {
+    outerArr.push(innerArr);
+  }
+
+  return outerArr;
+}
 
 module.exports = chunk;
