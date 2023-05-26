0f43ae66f561f3145ff4b33f4f345d6bb55780a0
MoMoZin <khinmomozin@gmail.com>
2023-05-26 12:22:16 -0600

    max char in a string method 1

--- maxchar/index.js
+++ maxchar/index.js
@@ -5,6 +5,30 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+// similar questions
+// What is the most common characher in the string?
+// Does String A have the same characters as String B (is it an anagram)?
+// Does the given string have any repeated characters in it?
+
+function maxChar(str) {
+  let charObj = {};
+  str.split("").forEach(char => {
+    if (!charObj[char]) {
+      charObj[char] = 1;
+    }
+    else {
+      charObj[char] += 1;
+    }
+  });
+  let maxCount = 0;
+  let result = "";
+  for (let curr in charObj) {
+    if (charObj[curr] > maxCount) {
+      maxCount = charObj[curr];
+      result = curr;
+    }
+  }
+  return result;
+}
 
 module.exports = maxChar;
