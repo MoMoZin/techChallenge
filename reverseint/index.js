4b245a00048e1bc2dc5f89b6caf2001ce9cbafb7
MoMoZin <khinmomozin@gmail.com>
2023-05-26 12:13:07 -0600

    reverse int method 2

--- reverseint/index.js
+++ reverseint/index.js
@@ -8,12 +8,19 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
+// //method one
+// function reverseInt(n) {
+//   if (n === 0) {
+//     return 0;
+//   }
+//   const reversedInt = parseInt(n.toString().split("").reverse().join(""));
+//   return n > 0 ? reversedInt : -reversedInt;
+// }
+
+//method two
 function reverseInt(n) {
-  if (n === 0) {
-    return 0;
-  }
-  const reversedInt = parseInt(n.toString().split("").reverse().join(""));
-  return n > 0 ? reversedInt : -reversedInt;
+  const reversed = n.toString().split("").reverse().join("");
+  return parseInt(reversed) * Math.sign(n);
 }
 
 module.exports = reverseInt;
