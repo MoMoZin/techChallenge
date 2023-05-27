d8479ae77c6b8a54203f9d244422fbe2cf76aa5d
MoMoZin <khinmomozin@gmail.com>
2023-05-27 16:28:23 -0600

    anagram second approach

--- anagrams/index.js
+++ anagrams/index.js
@@ -8,48 +8,86 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
+// // method 1
+// function anagrams(stringA, stringB) {
+
+//   //if two strings length is not the same, it is not anagram for sure
+//   if (stringA.length !== stringB.length) {
+//     return false;
+//   }
+
+//   //remove space and ! from both string and change to lowercase
+//   stringA = stringA.replace(/[^\w]/g, '').toLowerCase();
+//   stringB = stringB.replace(/[^\w]/g, '').toLowerCase();
+
+//   //create char map of stringA
+//   const stringAObj = {};
+//   stringA.split("").forEach(charA => {
+//     if (!stringAObj[charA]) {
+//       stringAObj[charA] = 1;
+//     }
+//     else {
+//       stringAObj[charA]++;
+//     }
+//   });
+
+//   //crate char map of stringB
+//   const stringBObj = {};
+//   stringB.split("").forEach(charB => {
+//     if (!stringBObj[charB]) {
+//       stringBObj[charB] = 1;
+//     }
+//     else {
+//       stringBObj[charB]++;
+//     }
+//   });
+
+//   //loop through stringAobj and check if each char exists in stringBobj as well
+//   let result = true;
+//   for (let charA in stringAObj) {
+//     if (!stringBObj[charA]) {
+//       result = false;
+//     }
+//   }
+//   return result;
+
+// }
+
+
 function anagrams(stringA, stringB) {
+
+  //remove space and ! from both string and change to lowercase
+  stringA = stringA.replace(/[^\w]/g, '').toLowerCase();
+  stringB = stringB.replace(/[^\w]/g, '').toLowerCase();
 
   //if two strings length is not the same, it is not anagram for sure
   if (stringA.length !== stringB.length) {
     return false;
   }
 
-  //remove space and ! from both string and change to lowercase
-  stringA = stringA.replace(/[^\w]/g, '').toLowerCase();
-  stringB = stringB.replace(/[^\w]/g, '').toLowerCase();
-
   //create char map of stringA
-  const stringAObj = {};
-  stringA.split("").forEach(charA => {
-    if (!stringAObj[charA]) {
-      stringAObj[charA] = 1;
-    }
-    else {
-      stringAObj[charA]++;
-    }
-  });
+  const stringAObj = createCharMap(stringA);
 
-  //crate char map of stringB
-  const stringBObj = {};
-  stringB.split("").forEach(charB => {
-    if (!stringBObj[charB]) {
-      stringBObj[charB] = 1;
-    }
-    else {
-      stringBObj[charB]++;
-    }
-  });
+  //create char map of stringB
+  const stringBObj = createCharMap(stringB);
 
   //loop through stringAobj and check if each char exists in stringBobj as well
-  let result = true;
   for (let charA in stringAObj) {
-    if (!stringBObj[charA]) {
-      result = false;
+    if (stringAObj[charA] !== stringBObj[charA]) {
+      return false;
     }
   }
-  return result;
+  return true;
 
 }
+//helper function to create char map multiple times
+const createCharMap = (str) => {
+  const charMap = {};
+  str.split("").forEach(char => {
+    charMap[char] = charMap[char] + 1 || 1;
+  });
+  return charMap;
+};
+
 
 module.exports = anagrams;
