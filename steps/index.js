abb514d66c048d10f41863f72711f2c78f5881b6
MoMoZin <khinmomozin@gmail.com>
2023-05-30 15:48:22 -0600

    printing step for loop approach

--- steps/index.js
+++ steps/index.js
@@ -17,6 +17,48 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+function steps(n) {
+  // // console.log("Current n: ", n);
+  // if (n === 1) {
+  //   console.log("#");
+  //   return;
+  // }
+
+  // if (n > 1) {
+  //   steps(n - 1);
+  //   let output = "";
+  //   for (let i = 1; i <= n; i++) {
+  //     output += '#';
+  //   }
+
+  //   // console.log("Current output: ", output);
+  //   console.log(output);
+  // }
+
+  // console.log('n: ', n);
+
+  if (n === 1) {
+    console.log("#");
+    return;
+  }
+
+  let textArr = [];
+
+  let text = '';
+  for (let i = 1; i <= n; i++) {
+    text += '#';
+    textArr.push(text);
+  }
+
+  textArr.forEach(t => {
+    const numOfSpace = n - t.length;
+    for (let j = 1; j <= numOfSpace; j++) {
+      t += " ";
+    }
+    console.log(t);
+  });
+
+
+}
 
 module.exports = steps;
